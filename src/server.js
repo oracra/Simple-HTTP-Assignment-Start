182af28faf7847be19af203de41af7c7d5dab9fa
oracra <nfa8750@rit.edu>
2021-02-07 13:56:15 -0500

    Added basic HTTP Server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,19 @@
 const http = require('http');
-console.log('hello world');
+const fs = require('fs');
 
-const helloFunciont = () => {
-    console.log('hello funciton')
-}
-
-//What the server needs
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
+// What the server needs
+const port = process.env.PORT || process.env.NODE_PORT || 3001;
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, response) => {
-    if(request.url === '/favicon.ico'){
-        console.log('favicon request');
-    }
+  if (request.url === '/favicon.ico') {
+    console.log(request.url);
+  }
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log('request received');
-
-    response.write("hi");
-    response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
 };
 
 http.createServer(onRequest).listen(port);
+console.log(`listening on 127.0.0.1:${port}`);
